75adcd41a1fc5f41b691d1f6a765d56f4c3f11c1
ViktorKW <73704875+ViktorKW@users.noreply.github.com>
2022-10-22 13:36:29 +0200

    added new styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { handleLocation, handleRoute } from './helpers/router';
 import './styles/index.scss';
-
+import "./styles/text.scss"
+import "./styles/button.scss"
 handleLocation();
 
 document.addEventListener('click', (e) => {
