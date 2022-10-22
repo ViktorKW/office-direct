2ed725132ff051a2e84fdd06d54bef10cdc02807
ViktorKW <73704875+ViktorKW@users.noreply.github.com>
2022-10-22 13:38:59 +0200

    added TestStyle to Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,7 @@
 import './style.scss';
 import Header from '../../components/Header';
 import testImg from '../../assets/test-1.jpg';
-
+import TestStyles from '../../components/TestStyles'
 const Home = () => {
   return `
     ${Header()}
@@ -9,6 +9,7 @@ const Home = () => {
       <h1>Homepage</h1>
       <img src="${testImg}" alt="/">
     </div>
+    ${TestStyles()}
   `;
 };
 
