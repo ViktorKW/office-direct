6c6d9b6d55449ecc710e4591c14b83b7c578eb56
ViktorKW <73704875+ViktorKW@users.noreply.github.com>
2022-10-22 13:37:32 +0200

    added TestStyles component for styles showcase

---
+++ src/components/TestStyles/index.js
@@ -0,0 +1,47 @@
+
+const TestStyles = () => {
+    return `<div style="background:#ffff; padding:10px;">
+        <h1>Styles showcase!</h1>
+        <hr/>
+        <h2>Buttons</h2>
+        <button class="blue-btn-big">class = "blue-btn-med"</button>
+        <br/>
+        <button class="blue-btn-med">class = "blue-btn-med"</button>
+        <br/>
+        <button class="blue-btn-small">class = "blue-btn-small"</button>
+        <br/>
+        <button class="grey-btn">class = "grey-btn"</button>
+        <br/>
+        <lable>class = "circle-btn"</lable>
+        <div style="display:flex">
+            <button class="circle-btn"></button>
+            <button class="circle-btn"></button>
+            <button class="circle-btn"><</button>
+            <button class="circle-btn">></button>
+        </div>
+        <hr/>
+        <h2>Text text</h2>
+        SimpleText
+        <header>header text</header>
+        <h1>h1 text</h1>
+        <h2>h2 text</h2>
+        <h3>h3 text</h3>
+        <h4>h4 text</h4>
+        <button>simple button text</button>
+        <br/>
+        <button class="btn-text-big">class="btn-text-big"</button>
+        <br/>
+        <button class="btn-text-med">class="btn-text-med"</button>
+        <br/>
+        <button class="btn-text-small">class="btn-text-small"</button>
+        <p>simple p text</p>
+        <p class="p-text-big">class="p-text-big</p>
+        <p class="p-text-med"> class="p-text-med"</p>
+        <p class="p-text-small">class="p-text-small"</p>
+        <caption>caption</caption>
+        <br/>
+        <lable>label</lable>
+    </div>`;
+};
+
+export default TestStyles;
