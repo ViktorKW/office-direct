e9b96b1313352ca9ad57437d7c15c88a0db4b9d7
Roman13456 <99034241+Roman13456@users.noreply.github.com>
2022-10-26 20:43:37 +0300

    updated footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,19 +10,32 @@ const Footer = () => {
 
   return `
     <footer>
-        <ul>
-            <li class='officeDirect'><img src='${footerLogo}'> <div><img src='${logoSign}'> <a href='#'>Кресла Samurai</a></div></li>
-            <li class='link'><a href='#' >О продукте</a></li>
-            <li class='link'><a href='#' >Отзывы</a></li>
-            <li class='link'><a href='#' >Доставка и оплата</a></li>
-            <li class='link mb-0 m-large'><a href='#'  >Контакты</a></li>
-            <li class='info number'><img src='${cellPhone}'><a href='#' >+7 (812) 600-31-24</a></li>
-            <li class='info'><img src='${mailBox}'><a href='#' >zakaz@kreslasamurai.ru</a></li>
-            <li class='info long'><img src='${jps}'><a href='#' >ул. Красного курсанта д.25, лит.в, оф. 114, бизнес-центр "RED CADET"</a></li>
-            <li style='margin-bottom:6px' class='privacyPolicy'><a href='#' >Политика конфиденциальности</a></li>
-            <li class='privacyPolicy'><a href='#' >© 2021 Все права защищены</a></li>
-        </ul>
-    
+    <div class='d-flex list justify-content-between'>
+        <div class='d-flex-column'>
+            <div class='officeDirect'>
+              <img src='${footerLogo}'> 
+              <div>
+                <img src='${logoSign}'>
+                <a href='#'>Кресла Samurai</a>
+              </div>
+            </div>
+            <p style='margin-bottom:6px' class='privacyPolicy hideOnMobile'><a href='#' >Политика конфиденциальности</a></p>
+            <p class='privacyPolicy hideOnMobile'><a href='#' >© 2021 Все права защищены</a></p>
+        </div>
+        <div class='d-flex-column'>
+            <p class='link'><a href='#' >О продукте</a></p>
+            <p class='link'><a href='#' >Отзывы</a></p>
+            <p class='link'><a href='#' >Доставка и оплата</a></p>
+        </div>
+        <div class='d-flex-column'>
+          <p class='link mb-0 m-large'><a href='#'  >Контакты</a></p>
+          <p class='info number'><img src='${cellPhone}'><a href='#' >+7 (812) 600-31-24</a></p>
+          <p class='info'><img src='${mailBox}'><a href='#' >zakaz@kreslasamurai.ru</a></p>
+          <p class='info long'><img src='${jps}'><a href='#' >ул. Красного курсанта д.25, лит.в, оф. 114, бизнес-центр "RED CADET"</a></p>
+          <p style='margin-bottom:6px' class='privacyPolicy hideOnDekstop'><a href='#' >Политика конфиденциальности</a></p>
+          <p class='privacyPolicy hideOnDekstop'><a href='#' >© 2021 Все права защищены</a></p>
+        </div>
+      </div>
     </footer>
   `;
 };
