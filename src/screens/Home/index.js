96fb3c0802628cd955164ee3e05474e91972e6ef
Sergii Kochetov <sergii.koch@gmail.com>
2022-10-27 23:55:50 +0300

    added Contact component

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,14 @@
 import './style.scss';
 import Header from '../../components/Header';
-import testImg from '../../assets/test-1.jpg';
 import Footer from '../../components/footer';
-import TestStyles from '../../components/TestStyles'
+import TestStyles from '../../components/TestStyles';
+import ContactUs from '../../components/ContactUs';
+
 const Home = () => {
   return `
     ${Header()}
-    <div class="hero home">
-      <h1>Homepage</h1>
-      <img src="${testImg}" alt="/">
-    </div>
     ${TestStyles()}
+    ${ContactUs()}
     ${Footer()}
   `;
 };
