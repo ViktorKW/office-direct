5e36fdbeb6fe80cff85d51ba71d4f07e2a64d72e
ViktorKW <viktorkw1201@gmail.com>
2022-10-30 21:31:01 +0200

    English translate

--- src/components/basketTotal/index.js
+++ src/components/basketTotal/index.js
@@ -1,7 +1,7 @@
 const BasketTotal = (init, current) => {
   return `
     <div>
-        <div class='total d-flex align-items-center'><p class='generalPrice'>Итого: <span class="oldPrice">${init} ₽ </span>${current} ₽</p></div>
+        <div class='total d-flex align-items-center'><p class='generalPrice'>Total: <span class="oldPrice">${init} ₽ </span>${current} ₽</p></div>
     </div>
   `;
 };
