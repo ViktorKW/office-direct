fc6e369799cc86d141cafae5755a6375ebc92392
ViktorKW <viktorkw1201@gmail.com>
2022-10-31 12:19:13 +0200

    added json products database

--- src/components/Catalog/components/Products/index.js
+++ src/components/Catalog/components/Products/index.js
@@ -1,59 +1,8 @@
 import "./style.scss";
 import Product from "./Product";
-import chair1 from "./images/chair1.svg";
-import chair2 from "./images/chair2.svg";
-const Products = () => {
-  const products_db = [
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair1,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-    {
-      name: "Samurai KL-1.04 dark-brown",
-      current_price: 25000,
-      full_price: 37000,
-      picture: chair2,
-    },
-  ];
+import products_db from "../../../../assets/products.json";
 
+const Products = () => {
   function drawProducts(db) {
     console.log(db);
     const result = db.map((element) => Product(element));
