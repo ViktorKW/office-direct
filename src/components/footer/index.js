c32ae4ac19f5f1ed61b2b99691fb3f2ed85ffa4e
ViktorKW <viktorkw1201@gmail.com>
2022-11-02 20:08:49 +0200

    translated to English

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -15,25 +15,26 @@ const Footer = () => {
             <div class='officeDirect'>
               <img src='${footerLogo}'> 
               <div>
-                <img src='${logoSign}'>
-                <a href='#'>Кресла Samurai</a>
+                <strong>OfficeDirect</strong>
+                <a href='#'>Armchairs Samurai</a>
               </div>
             </div>
-            <p style='margin-bottom:6px' class='privacyPolicy hideOnMobile'><a href='#' >Политика конфиденциальности</a></p>
-            <p class='privacyPolicy hideOnMobile'><a href='#' >© 2021 Все права защищены</a></p>
+            <p style='margin-bottom:6px' class='privacyPolicy hideOnMobile'><a href='#' >Privacy policy</a></p>
+            <p class='privacyPolicy hideOnMobile'><a href='#' >© 2021 All rights reserved</a></p>
         </div>
         <div class='d-flex-column'>
-            <p class='link'><a href='#' >О продукте</a></p>
-            <p class='link'><a href='#' >Отзывы</a></p>
-            <p class='link'><a href='#' >Доставка и оплата</a></p>
+            <p class='link'><a href='#' >About</a></p>
+            <p class='link'><a href='#' >Reviews</a></p>
+            <p class='link'><a href='#' >Delivery and payment</a></p>
+            <p class='link mb-0 m-large'><a href='#'>Contacts</a></p>
         </div>
+
         <div class='d-flex-column'>
-          <p class='link mb-0 m-large'><a href='#'  >Контакты</a></p>
           <p class='info number'><img src='${cellPhone}'><a href='#' >+7 (812) 600-31-24</a></p>
-          <p class='info'><img src='${mailBox}'><a href='#' >zakaz@kreslasamurai.ru</a></p>
-          <p class='info long'><img src='${jps}'><a href='#' >ул. Красного курсанта д.25, лит.в, оф. 114, бизнес-центр "RED CADET"</a></p>
-          <p style='margin-bottom:6px' class='privacyPolicy hideOnDekstop'><a href='#' >Политика конфиденциальности</a></p>
-          <p class='privacyPolicy hideOnDekstop'><a href='#' >© 2021 Все права защищены</a></p>
+          <p class='info'><img src='${mailBox}'><a href='#' >zakaz@kreslasamurai.com</a></p>
+          <p class='info long'><img src='${jps}'><a href='#' >Robert Robertson, 1234 NW Bobcat Lane, St. Robert, MO 65584-5678</a></p>
+          <p style='margin-bottom:6px' class='privacyPolicy hideOnDekstop'><a href='#' >Privacy policy</a></p>
+          <p class='privacyPolicy hideOnDekstop'><a href='#' >© 2021 All rights reserved</a></p>
         </div>
       </div>
     </footer>
