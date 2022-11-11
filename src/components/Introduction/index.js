91f2703fd58a410cda64cbd959111b23b5ce875d
ViktorKW <viktorkw1201@gmail.com>
2022-11-11 19:01:03 +0200

    reactify Introduction component

--- src/components/Introduction/index.js
+++ src/components/Introduction/index.js
@@ -1,22 +1,23 @@
-import "./style.scss";
-import big_chair from "./images/big_chair.svg";
-import small_chair from "./images/small_chair.svg";
-import ukraine from "./images/ukraine-svgrepo-com.svg";
+import React from 'react';
+import './style.scss';
+import big_chair from './images/big_chair.svg';
+import small_chair from './images/small_chair.svg';
+import ukraine from './images/ukraine-svgrepo-com.svg';
 const Introduction = () => {
-  return `
-    <div class = "introduction">
-      <div class = "info">
-        <h1>Armchairs Samurai on favorable terms <u>throughout</u> the country<img src = "${ukraine}"/></h1>
-        <p>
-          Here you do not have to blindly buy goods - test before buying
-        </p>
-        <button class = "blue-btn">
-        Sign up for a test drive</button>
+  return (
+    <div className='introduction'>
+      <div className='info'>
+        <h1>
+          Armchairs Samurai on favorable terms <u>throughout</u> the country
+          <img src={ukraine} alt='ukraine flag' />
+        </h1>
+        <p>Here you do not have to blindly buy goods - test before buying</p>
+        <button className='blue-btn'>Sign up for a test drive</button>
       </div>
-      <img src = "${big_chair}"/>
-      <img src = "${small_chair}"/>
+      <img src={big_chair} alt='big chair' />
+      <img src={small_chair} alt='small chair' />
     </div>
-  `;
+  );
 };
 
 export default Introduction;
