6ecd8057e1afde10a89df531827af276fb0b594a
ViktorKW <viktorkw1201@gmail.com>
2022-11-11 18:05:48 +0200

    reactify Advantages component

--- src/components/Advantages/index.js
+++ src/components/Advantages/index.js
@@ -1,28 +1,36 @@
-import "./style.scss";
-import trolley from "./icons/trolley.svg";
-import chess from "./icons/chess.svg";
-import discount from "./icons/discount.svg";
+import React from 'react';
+import './style.scss';
+import trolley from './icons/trolley.svg';
+import chess from './icons/chess.svg';
+import discount from './icons/discount.svg';
 
 const Advantages = () => {
-  return `
-    <div class = "advantages">
-      <div class = "feature">
-        <img src=${trolley} alt = "not found">
-        <p><strong>Free shipping</strong> across Kyiv and Odesa — minimum 3 products</p>
-        <a href="#">How to get one?</a>
+  return (
+    <div className='advantages'>
+      <div className='feature'>
+        <img src={trolley} alt='not found' />
+        <p>
+          <strong>Free shipping</strong> across Kyiv and Odesa — minimum 3
+          products
+        </p>
+        <a href='#'>How to get one?</a>
       </div>
-      <div class = "feature">
-        <img src=${chess} alt = "not found">
-        <p><strong>Individual discounts</strong> when ordering from 3 products</p>
-        <a href="#">Check conditions</a>
+      <div className='feature'>
+        <img src={chess} alt='not found' />
+        <p>
+          <strong>Individual discounts</strong> when ordering from 3 products
+        </p>
+        <a href='#'>Check conditions</a>
       </div>
-      <div class = "feature">
-        <img src=${discount} alt = "not found">
-        <p><strong>Special conditions</strong> apply for legal entities</p>
-        <a href="#">Learn more</a>
+      <div className='feature'>
+        <img src={discount} alt='not found' />
+        <p>
+          <strong>Special conditions</strong> apply for legal entities
+        </p>
+        <a href='#'>Learn more</a>
       </div>
     </div>
-  `;
+  );
 };
 
 export default Advantages;
