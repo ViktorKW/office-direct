c545b57be77c6dbebae044d9a57070932edd42f2
ViktorKW <viktorkw1201@gmail.com>
2022-11-11 17:53:21 +0200

    reactify header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,50 +1,63 @@
+import React from 'react';
 import './style.scss';
 import cellPhone from '../../assets/icons/cellPhone.svg';
 import send from '../../assets/icons/send.svg';
 import cart from '../../assets/icons/cart.svg';
 
 const Header = () => {
-  return `
-    <header class="header">
-      <div class="header__main">
-        <div class="logo"><span class="logo__beggining">Office</span><span class="logo__end">Direct</span></div>
-        <input type="text" placeholder="Search..." class="input">
-        <div class="phone"><img src='${cellPhone}' alt="cellphone-icon" />+38(095)555-55-55</div>
-        <button type="button" class="button">Request a call</button>
+  return (
+    <header className='header'>
+      <div className='header__main'>
+        <div className='logo'>
+          <span className='logo__beggining'>Office</span>
+          <span className='logo__end'>Direct</span>
+        </div>
+        <input type='text' placeholder='Search...' className='input' />
+        <div className='phone'>
+          <img src={cellPhone} alt='cellphone-icon' />
+          +38(095)555-55-55
+        </div>
+        <button type='button' className='button'>
+          Request a call
+        </button>
       </div>
-      <div class="header__menu">
-        <div class="header__menu-main">
-          <div class="button">
-            <span class="material-symbols-outlined">
-              sort
-            </span>
-            <button type="button" class="button__catalogue">Catalogue</button>
+      <div className='header__menu'>
+        <div className='header__menu-main'>
+          <div className='button'>
+            <span className='material-symbols-outlined'>sort</span>
+            <button type='button' className='button__catalogue'>
+              Catalogue
+            </button>
           </div>
-          <nav class="header__nav">
-            <a href="#">About the products</a>
-            <a href="#">Testimonials</a>
-            <a href="#">Delivery & payment</a>
-            <a href="#">Contacts</a>
+          <nav className='header__nav'>
+            <a href='#'>About the products</a>
+            <a href='#'>Testimonials</a>
+            <a href='#'>Delivery & payment</a>
+            <a href='#'>Contacts</a>
           </nav>
         </div>
-        <div class="header__menu-additional">
-          <div class="location">
-            <img src='${send}' alt="send icon" />
-            <select name="city" id="city">
-              <option value="kyiv" selected>Kyiv</option>
-              <option value="london">London</option>
-              <option value="berlin">Berlin</option>
+        <div className='header__menu-additional'>
+          <div className='location'>
+            <img src={send} alt='send icon' />
+            <select name='city' id='city'>
+              <option value='kyiv' selected>
+                Kyiv
+              </option>
+              <option value='london'>London</option>
+              <option value='berlin'>Berlin</option>
             </select>
           </div>
-          <div class="cart">
-            <img src='${cart}' alt="cart icon" />
-            <a href='/cart' data-link>My orders</a>
-            <span class="quantity">+1</span>
+          <div className='cart'>
+            <img src={cart} alt='cart icon' />
+            <a href='/cart' data-link>
+              My orders
+            </a>
+            <span className='quantity'>+1</span>
           </div>
         </div>
       </div>
     </header>
-  `;
+  );
 };
 
 export default Header;
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,26 +1,24 @@
-import "./style.scss";
-import Header from "../../components/Header";
-import Catalog from "../../components/Catalog";
-import Introduction from "../../components/Introduction";
-import SamuraiLady from "../../components/SamuraiLady";
-import Description from "../../components/Description";
-import Clients from "../../components/Clients";
-import ContactUs from "../../components/ContactUs";
-import Footer from "../../components/footer";
+import React from 'react';
+import Header from '../../components/Header';
+import './style.scss';
+// import Header from '../../components/Header';
+// import Catalog from '../../components/Catalog';
+// import Introduction from '../../components/Introduction';
+// import SamuraiLady from '../../components/SamuraiLady';
+// import Description from '../../components/Description';
+// import Clients from '../../components/Clients';
+// import ContactUs from '../../components/ContactUs';
+// import Footer from '../../components/footer';
 
 const Home = () => {
-  return `
-    <div class = "home-page">
-      ${Header()}
-      ${Introduction()}
-      ${Catalog()}
-      ${Description()}
-      ${SamuraiLady()}
-      ${Clients()}
-      ${ContactUs()}
-      ${Footer()}
+  return (
+    <div class='home-page'>
+      <Header />
     </div>
-  `;
+  );
 };
 
 export default Home;
+
+// ${Header()}${Introduction()}${Catalog()}${Description()}${SamuraiLady()}$
+// {Clients()}${ContactUs()}${Footer()}
